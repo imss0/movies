0ad3f05e9ed0abba0abda968bcd1239ef236f60c
sohyun <shlim0410@gmail.com>
2023-06-13 13:41:38 +0100

    feat: render movie list in home page

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,6 @@
+import { Link, useMatch } from "react-router-dom";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const NavContainer = styled.div`
   max-width: 350px;
@@ -11,14 +13,47 @@ const NavText = styled.p`
   font-size: 16px;
   font-weight: 600;
   text-transform: uppercase;
+  position: relative;
+  display: flex;
+  justify-content: center;
+`;
+
+const Indicator = styled(motion.span)`
+  width: 8px;
+  height: 8px;
+  position: absolute;
+  background-color: tomato;
+  border-radius: 50%;
+  bottom: -12px;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
 `;
 
 export default function Nav() {
+  const homeMatch = useMatch("/");
+  const upcomingMatch = useMatch("coming-soon");
+  const nowplayingMatch = useMatch("now-playing");
   return (
     <NavContainer>
-      <NavText>popular</NavText>
-      <NavText>coming soon</NavText>
-      <NavText>now playing</NavText>
+      <NavText>
+        <Link to="/">
+          popular
+          {homeMatch && <Indicator layoutId="indicator" />}
+        </Link>
+      </NavText>
+      <NavText>
+        <Link to="/coming-soon">
+          coming soon
+          {upcomingMatch && <Indicator layoutId="indicator" />}
+        </Link>
+      </NavText>
+      <NavText>
+        <Link to="/now-playing">
+          now playing
+          {nowplayingMatch && <Indicator layoutId="indicator" />}
+        </Link>
+      </NavText>
     </NavContainer>
   );
 }
--- src/pages/Popular.tsx
+++ src/pages/Popular.tsx
@@ -1,3 +1,42 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { useQuery } from "@tanstack/react-query";
+import { getPopular, makeImagePath, IMovieDetail } from "../api";
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Img = styled(motion.img)`
+  width: 200px;
+  border-radius: 20px;
+`;
+
+const ImgVariants = {
+  hover: { scale: 1.2, y: -30 },
+};
+
 export default function Popular() {
-  return <div>Popular movies</div>;
+  const { isLoading, data } = useQuery(["movies", "popular"], getPopular);
+  const movies = data?.results;
+
+  return (
+    <div>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        movies.map((movie: IMovieDetail) => (
+          <Img
+            variants={ImgVariants}
+            whileHover="hover"
+            src={makeImagePath(movie.poster_path)}
+            key={movie.id}
+          />
+        ))
+      )}
+    </div>
+  );
 }
